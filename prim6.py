36f1801de26c02de58ad6744f46bb7671075ff01
Nikita Strelchenko <nickita.strelchenko@yandex.ru>
2018-10-01 09:31:01 +0300

    123

--- prim6.py
+++
@@ -1,8 +0,0 @@
-#Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
-
-
-import string
-x = int(input("Введите номер буквы:"))
-alf = string.ascii_lowercase
-letter= alf[x]
-print("Ваша буква:",letter)
